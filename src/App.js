f077f2813f1ce4b41ed8436a4716f06136cc4e53
sunshine-2025 <215694116@qq.com>
2025-02-09 16:56:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './home-page/index';
-import ResultPage from './result-analysis-page/index';
+import './App.css';
 
 function App() {
+  const handleStartTest = () => {
+    window.location.href = 'home-page/index.html'; // 跳转到首页
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/result-analysis-page" element={<ResultPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>欢迎来到爱情兼容性测试网站</h1>
+        <p>在这里，你可以测试你和另一半的兼容性！</p>
+        <button onClick={handleStartTest}>开始测试</button> {/* 添加开始测试按钮 */}
+      </header>
+    </div>
   );
 }
 
