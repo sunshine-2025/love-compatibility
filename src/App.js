e60c3bd3346918d3a6b296e911fa119a5959bbf1
sunshine-2025 <215694116@qq.com>
2025-02-09 16:41:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>欢迎来到爱情兼容性测试网站</h1>
+        <p>在这里，你可以测试你和另一半的兼容性！</p>
       </header>
     </div>
   );
